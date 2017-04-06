addba162c6f9e6e9147e85c864245cb96bc4192e
Balint Dorner <dorner.balint@gmail.com>
2017-04-06 13:50:06 +0200

    help function called in class Control

--- todo-app.py
+++ todo-app.py
@@ -1,8 +1,18 @@
 import sys
 
 class Control():
-    pass
 
+    def arg_reader(self):
+        self.list_argv = []
+        if len(sys.argv) <= 1:
+            self.list_argv = []
+        else:
+            self.list_argv = sys.argv[1:]
+
+        if len(self.list_argv) == 0:
+            display.print_help()
+        else:
+            pass
 
 class Model():
     pass
@@ -11,8 +21,10 @@ class Model():
 
 class Display():
 
-    def print_help():
+    def print_help(self):
         print("Python Todo application \n ======================= \n \n Command line arguments: \n -l   Lists all the tasks \n -a   Adds a new task \n -r   Removes an task \n -c   Completes an task")
 
 control = Control()
 display = Display()
+
+control.arg_reader()
