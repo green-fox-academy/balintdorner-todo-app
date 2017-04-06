f82512d0150e3b61d18333aa69e03041a8aecbf4
Balint Dorner <dorner.balint@gmail.com>
2017-04-06 13:37:51 +0200

    function: print help-created

---
+++ todo-app.py
@@ -0,0 +1,18 @@
+import sys
+
+class Control():
+    pass
+
+
+class Model():
+    pass
+
+
+
+class Display():
+
+    def print_help():
+        print("Python Todo application \n ======================= \n \n Command line arguments: \n -l   Lists all the tasks \n -a   Adds a new task \n -r   Removes an task \n -c   Completes an task")
+
+control = Control()
+display = Display()
