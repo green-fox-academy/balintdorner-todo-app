515d7f901aabe47ce5edf9fa67c83e1343cf0740
Balint Dorner <dorner.balint@gmail.com>
2017-04-06 15:42:54 +0200

    print list elements from the file

--- todo-app.py
+++ todo-app.py
@@ -2,29 +2,50 @@ import sys
 
 class Control():
 
-    def arg_reader(self):
+    def __init__(self):
         self.list_argv = []
+        self.arg_reader()
+        self.controller()
+        display.print_file()
+
+    def arg_reader(self):
         if len(sys.argv) <= 1:
             self.list_argv = []
         else:
             self.list_argv = sys.argv[1:]
 
+    def controller(self):
         if len(self.list_argv) == 0:
             display.print_help()
         else:
             pass
 
 class Model():
-    pass
 
+    def __init__(self):
+        self.txt = ""
+        self.adder()
+#        self.txt_dict =
 
+    def adder(self):
+        my_file = open('database.txt', 'r')
+        self.txt = my_file.readlines()
+        my_file.close()
 
 class Display():
 
     def print_help(self):
         print("Python Todo application \n ======================= \n \n Command line arguments: \n -l   Lists all the tasks \n -a   Adds a new task \n -r   Removes an task \n -c   Completes an task")
 
-control = Control()
-display = Display()
+    def print_file(self):
+        if len(model.txt) == 0:
+            print("No todos for today! :)")
+        else:
+            for i in range(len(model.txt)):
+                print(i+1, model.txt[i][:-1])
 
-control.arg_reader()
+
+
+model = Model()
+display = Display()
+control = Control()
